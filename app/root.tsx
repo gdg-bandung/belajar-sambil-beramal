c27625089c7f15eb5b896866e641ca837fbf8e80
fikrialwan <fikriar93@gmail.com>
2026-02-19 23:06:20 +0700

    feat: add favicon and meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -37,10 +37,14 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Belajar Sambil Beramal</title>
+        <meta name="description" content="Tingkatkan skill Anda dengan materi belajar berkualitas di Belajar Sambil Beramal. Dapatkan akses edukasi terbaik sekaligus berdonasi untuk program kemanusiaan."></meta>
         <meta name="robots" content="index, follow"></meta>
         <link rel="canonical" href="https://bsb.gdgbandung.com/" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <Meta />
         <Links />
       </head>
